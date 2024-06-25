9ff515334ca2bb592c096ce5739299435fbefbdd
SpandanPriyam22 <spandanpriyam1976@gmail.com>
2024-06-25 17:10:34 +0530

    Files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import ProjectSidebar from "./components/ProjectsSidebar";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
   return (
-    <main className="h-screen my-8">
-      <ProjectSidebar/>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar />
+      <NoProjectSelected />
     </main>
   );
 }
